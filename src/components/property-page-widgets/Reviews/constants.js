bb52f9ad6747eacaa74407eb9bd93dda6761ef6f
red-runner <pol@lodgify.com>
2019-03-06 17:19:12 +0100

    feat(Reviews): consume maxCharacters for the required inputs

--- src/components/property-page-widgets/Reviews/constants.js
+++ src/components/property-page-widgets/Reviews/constants.js
@@ -13,3 +13,9 @@ const NUMBER_OF_PLACEHOLDERS = 3;
 export const PLACEHOLDERS = Array(NUMBER_OF_PLACEHOLDERS).fill(
   PLACEHOLDER_DATA
 );
+
+export const COMMENT_MAX_LENGTH = 800;
+export const EMAIL_MAX_LENGTH = 255;
+export const LOCATION_MAX_LENGTH = 255;
+export const NAME_MAX_LENGTH = 80;
+export const TITLE_MAX_LENGTH = 30;
